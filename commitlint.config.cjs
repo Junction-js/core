4bd125e41b2f8249b2361d2d8190ef0bf2e7981a
krebinkkj <98119787+krebinkkj@users.noreply.github.com>
2025-09-03 15:40:27 +0000

    fix: commitlint.config.cjs

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-max-line-length': [2, 'always', 100]
+    },
+    ignores: [(message)] => message.includes('[skip ci]')
 }
